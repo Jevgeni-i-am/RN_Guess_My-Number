7985bb7f9e118bdae872b606c8acc4dd01bb936a
Jevgeni <67290830+Jevgeni-i-am@users.noreply.github.com>
2023-04-19 18:33:54 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+
+
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
+import Colors from './constant/colors';
+import GameOverScreen from './screens/GameOverScreen';
+import GameScreen from './screens/GameScreen';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState()
+  const [gameIsOver, setGameIsOver] = useState(true)
+  const [guessRounds, setGuessRounds] = useState(0)
+
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber)
+    setGameIsOver(false)
+  }
+
+  function gameOverHandler(numberOfRounds) {
+    setGameIsOver(true)
+    setGuessRounds(numberOfRounds)
+  }
+
+  function startNewGameHandler() {
+    setUserNumber(null)
+    setGuessRounds(0)
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />
+
+  // Check state and forward to screens according to state
+  if (userNumber) {
+    screen = <GameScreen
+      userNumber={userNumber}
+      onGameOver={gameOverHandler}
+    />
+  }
+  if (gameIsOver && userNumber) {
+    screen = <GameOverScreen
+      userNumber={userNumber}
+      roundsNumber={guessRounds}
+      onStartNewGame={startNewGameHandler}
+    //pickedNumber={pickedNumber} 
+    />
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <LinearGradient
+        colors={[Colors.primary700, Colors.accent500]}
+        style={styles.container}>
+        <ImageBackground source={require('./assets/images/bg.png')}
+          resizeMode="cover"
+          style={styles.container}
+          imageStyle={styles.backgroundImage}
+        >
+          <SafeAreaView style={styles.container}>
+            {screen}
+          </SafeAreaView>
+        </ImageBackground>
+      </LinearGradient>
+
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  backgroundImage: {
+    opacity: 0.35
+  }
 });
